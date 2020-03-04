90584e7236e47ae6eecc1f5f7dcc7df88a57c685
Scott Bromander <scottbromander@gmail.com>
2020-03-03 21:33:27 -0600

    wired up graphql

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("GraphQL is amazeballs!");
 });
+
+const root = { hello: () => `Hello, I am Scott` };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
 
 app.listen(8080, () => {
   console.log("Running on Port 8080!");
